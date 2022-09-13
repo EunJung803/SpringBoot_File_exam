43364f90657b1106d6062de2e8422d387ee184ec
EunJung803 <aimee3322@gmail.com>
2022-09-13 13:32:08 +0900

    Test : 회원 수 test, 개발DB용 샘플 데이터 작업, Test용 DB 설정

--- src/test/java/com/ll/exam/app10/AppTests.java
+++ src/test/java/com/ll/exam/app10/AppTests.java
@@ -1,16 +1,20 @@
 package com.ll.exam.app10;
 
 import com.ll.exam.app10.app.home.controller.HomeController;
+import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -19,11 +23,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional  // 이 테스트를 수행하며 생긴 DB 변동사항은 반영되지 않는다.
 @AutoConfigureMockMvc
+@ActiveProfiles({"base-addi", "test"})
 public class AppTests {
 
     @Autowired
     private MockMvc mvc;    // MockMvc == 호출기, 브라우저라고 생각하면 된다.
     // 특정 요청을 발생시킴
+
+    @Autowired
+    private MemberService memberService;
 
     @Test
     @DisplayName("메인화면에서는 안녕이 나와야 한다.")
@@ -39,5 +47,13 @@ public class AppTests {
                 .andExpect(handler().handlerType(HomeController.class))
                 .andExpect(handler().methodName("main"))
                 .andExpect(content().string(containsString("안녕")));
+    }
+
+    @Test
+    @DisplayName("회원의 수")
+    @Rollback(false)
+    void t2() throws Exception {
+        long count = memberService.count();
+        assertThat(count).isGreaterThan(0);
     }
 }

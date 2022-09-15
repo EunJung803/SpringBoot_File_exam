c115b921613b7af95a4f15e340d162804f833c64
EunJung803 <aimee3322@gmail.com>
2022-09-15 15:12:39 +0900

    Feat : 개발모드에서 기본적으로 생성되는 회원이 프로필이미지를 가지도록

--- src/main/java/com/ll/exam/app10/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app10/app/member/service/MemberService.java
@@ -34,7 +34,7 @@ public class MemberService implements UserDetailsService {
     }
 
     public Member join(String username, String password, String email, MultipartFile profileImg) {
-        String profileImgDirName = "member/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
+        String profileImgDirName = getCurrentProfileImgDirName();
 
         String ext = Util.file.getExt(profileImg.getOriginalFilename());
 
@@ -100,5 +100,15 @@ public class MemberService implements UserDetailsService {
         member.setProfileImg(null);     // 프로파일 이미지를 null로 설정
 
         memberRepository.save(member);  // 변경사항 저장
+    }
+
+    private String getCurrentProfileImgDirName() {
+        return "member/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
+    }
+
+    public void setProfileImgByUrl(Member member, String url) {
+        String filePath = Util.file.downloadImg(url, genFileDirPath + "/" + getCurrentProfileImgDirName() + "/" + UUID.randomUUID());
+        member.setProfileImg(getCurrentProfileImgDirName() + "/" + new File(filePath).getName());
+        memberRepository.save(member);
     }
 }
